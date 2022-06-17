a3439e6a5bc82594741ae1bdf5cbf328431f5ee2
cedric karungu <ckarungu921@gmail.com>
2022-06-17 15:12:02 +0200

    feat: build dynamicky questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,15 @@ const emailInput = document.getElementById('email');
 const errorNameOutput = document.querySelector('.error-name');
 const errorEmailOutput = document.querySelector('.error-email');
 
+/** ________ QUESTION PAGE ELEMENTS________ */
+const questionDetail = document.getElementById('question-detail');
+const times = document.getElementById('times');
+const progressBar = document.querySelector('progress-bar');
+const questionForm = document.getElementById('questionForm');
+const assertions = document.getElementById('assertions');
+
 const user = new User('', '');
+let questionId = 0;
 
 /** Wait for event to login form */
 loginForm.addEventListener('submit', (e) => {
@@ -28,6 +36,31 @@ loginForm.addEventListener('submit', (e) => {
 		//if correct we display question page
 		loginPage.style.display = 'none';
 		questionPage.style.display = 'block';
+		questionHandler(questionId);
 	}
 	console.log(user);
 });
+
+questionForm.addEventListener('click', (e) => {
+	e.preventDefault();
+	questionId++;
+	questionHandler(questionId);
+});
+
+const questionHandler = (id) => {
+	if (questions[id]) {
+		//we display first a question
+		assertions.innerHTML = '';
+		questionDetail.textContent = questions[id].titre;
+		questions[id].assertions.forEach((ass) => {
+			assertions.innerHTML += `<div class="form-group-question">
+            <input type="radio" name="nom" value="${ass}" required />
+            <label for="nom">${ass}</label>
+          </div>`;
+		});
+	} else {
+		resultWinPage.style.display = 'block';
+		loginPage.style.display = 'none';
+		questionPage.style.display = 'none';
+	}
+};
--- assets/js/models/question.js
+++ assets/js/models/question.js
@@ -1,12 +1,28 @@
-class Question {
-	constructor(titre, answer) {
-		this.titre = titre;
-		this.answer = answer;
-		this.assertions = [];
-	}
+// class Question {
+// 	constructor(titre, answer) {
+// 		this.titre = titre;
+// 		this.answer = answer;
+// 		this.assertions = [];
+// 	}
 
-	//  question.addAssertion({})
-	addAssertion(label, value) {
-		this.assertions.push({ label: label, value: value });
-	}
-}
+// 	//  question.addAssertion({})
+// 	addAssertion(label, value) {
+// 		this.assertions.push(label, value);
+// 	}
+// }
+
+const questions = [
+	{
+		id: 1,
+		titre: 'extension du fichier js',
+		answer: '.js',
+		assertions: ['.js', '.jsx', '.python'],
+	},
+	{
+		id: 2,
+		titre: 'un framework php',
+		answer: '.js',
+		assertions: ['express', 'rails', 'laravel','spring '],
+	},
+];
+
