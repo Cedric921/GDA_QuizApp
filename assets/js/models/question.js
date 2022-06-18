daa7f42202599f767c656947cf1d40c3d0936799
cedric karungu <ckarungu921@gmail.com>
2022-06-18 12:06:45 +0200

    feat: update questions

--- assets/js/models/question.js
+++ assets/js/models/question.js
@@ -20,9 +20,33 @@ const questions = [
 	},
 	{
 		id: 4,
-		titre: 'un framework php',
-		answer: 'laravel',
-		assertions: ['express', 'rails', 'laravel', 'spring '],
+		titre: 'What is the correct HTML element for inserting a line break?',
+		answer: 'br',
+		assertions: ['lb', 'break', 'br', 'bk'],
 	},
+	{
+		id: 5,
+		titre: 'Choose the correct tag for the largest heading:',
+		answer: 'h6',
+		assertions: ['h6', 'heading', 'h1', 'head', 'p'],
+	},
+	{
+		id: 6,
+		titre: 'JavaScript uses the ___ keyword to define variables',
+		answer: 'let',
+		assertions: ['int', 'float', 'for', 'let', 'struct'],
+	},
+	{
+		id: 7,
+		titre: 'What is RAM?',
+		answer: 'Random Access Memory',
+		assertions: [
+			'Recycled Ability Memory',
+			'Routing Automatic Memory',
+			'Routed Access Memory',
+			'Random Access Memory',
+		],
+	},
+
 ];
 
